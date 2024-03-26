b23e0630a20a94cea58f7a239bcc4ba9a71e862c
mikaelmotin <motin@chalmers.se>
2024-03-26 16:25:37 +0100

    project setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    // Javascript goes here
+</script>
+
+<!-- HTML code for page -->
+<div class=" bg-gradient-to-br from-emerald-200 to-blue-200 w-screen h-screen">
+    <div class="flex w-full h-full justify-center items-center flex-col">
+        <p class="text-5xl font-semibold text-black mb-10">Quick Project Setup Using</p>
+        <p class="text-3xl font-semibold text-black animate-pulse">Vite</p>
+        <p class="text-3xl font-semibold text-black animate-pulse">Svelte</p>
+        <p class="text-3xl font-semibold text-black animate-pulse">Tailwind CSS</p>
+
+        <div class="mt-10 flex gap-x-8">
+            <a href="/example1">
+                <button class="bg-blue-500 hover:bg-blue-700 hover:scale-110 duration-300 text-white font-bold py-2 px-4 rounded-full">
+                    Example 1
+                </button>
+            </a>
+            <a href="/example2">
+                <button class="bg-blue-500 hover:bg-blue-700 hover:scale-110 duration-300 text-white font-bold py-2 px-4 rounded-full">
+                    Example 2
+                </button>
+            </a>
+        </div>
+    </div>
+
+</div>
+
+
+<style>
+    /* If not using Tailwinds, normal CSS can be used here */
+</style>
---
+++ src/routes/example2/+page.svelte
@@ -0,0 +1,8 @@
+<p>Welcome to Example 2</p>
+
+<a href="/">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+    Button to go Home
+    </button>
+</a>
+
